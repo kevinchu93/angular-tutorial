d40bbed8d21aaeada74d31ccf3d97c6fe572b177
Kevin Chu <kevinxchu@gmail.com>
2017-08-23 13:45:19 -0700

    worked through almost entire tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }         from '@angular/core';
+import { BrowserModule }    from '@angular/platform-browser';
+import { FormsModule }      from '@angular/forms';  //  <-- NgModel lives here
+import { HttpModule }       from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule }     from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,     // <-- import the FormsModule before binding with [(ngModel}]
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        HeroDetailComponent,
+        HeroesComponent,
+        DashboardComponent
+    ],
+    providers: [ HeroService ],
+    bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+
+export class AppModule {}
